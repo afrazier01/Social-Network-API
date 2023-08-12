f54577161031d2e03fd24e9ffbe43b92baf0dd62
afrazier01 <amfrazier01@gmail.com>
2023-08-12 14:20:07 -0500

    User routes working well

--- utils/seed.js
+++ utils/seed.js
@@ -6,6 +6,37 @@ User
         username: 'testUser01',
         email: 'testuser01@test.com',
     })
+    .then(result => console.log(`New user created!` , result))
+    .catch(err => console.log(err));
+
+User
+    .create({
+        username: 'testUser02',
+        email: 'testuser2@test.com',
+    })
+    .then(result => console.log('New user created!', result))
+    .catch(err => console.log(err));
+
+User
+    .create({
+        username: 'IamThaBest033',
+        email: 'bestuser4ever@mail.com',
+    })
     .then(result => console.log('New user created!', result))
     .catch(err => console.log(err));
   
+User
+    .create({
+        username: 'Rugrats00',
+        email: 'candyland@umail.com',
+    })
+    .then(result => console.log('New user created!', result))
+    .catch(err => console.log(err));
+
+User
+    .create({
+        username: 'Greatest2EverDoIt',
+        email: 'greatest@vmail.com',
+    })
+    .then(result => console.log('New user created!', result))
+    .catch(err => console.log(err));
